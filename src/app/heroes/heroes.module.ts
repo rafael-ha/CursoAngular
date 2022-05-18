5c7ff6cbfd12d90c039e311ecbe2e0fc6f03f129
Rafael Herrera Amedey <rafael.herrera@alten.es>
2022-05-18 09:21:46 +0200

    HeroesApp

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
+import { FormsModule } from '@angular/forms';
+import { ConfirmarComponent } from './components/confirmar/confirmar.component';
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    HeroeComponent,
+    HomeComponent,
+    ListadoComponent,
+    BuscarComponent,
+    HeroeTarjetaComponent,
+    ImagenPipe,
+    ConfirmarComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    FlexLayoutModule,
+    HeroesRoutingModule
+  ]
+})
+export class HeroesModule { }
---
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscar',
+  templateUrl: './buscar.component.html',
+  styles: [],
+})
+export class BuscarComponent {
+  termino: string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe |undefined;
+  constructor(private heroeService: HeroesService) {}
+
+  buscando() {
+    this.heroeService.getSugerencias(this.termino.trim()).subscribe({
+      next: (heroes) => {
+        this.heroes = heroes;
+      },
+    });
+  }
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    if(!event.option.value){
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+    this.heroeService.getHeroePorId(heroe.id!)
+    .subscribe({
+      next:(heroe)=>{this.heroeSeleccionado = heroe}
+    })
+  }
+}
